a1e105fea7669054ce512f412a8f3aadfe0239cb
matanki.saito <matanki.saito@gmail.com>
2020-01-26 22:46:12 +0900

    機能追加と修正
     - タブをまとめた
     - 翻訳を修正
     - 一時ファイル作成が失敗するのでコードを修正
     - I:RのDLLを追加できるようにした
     - ログをファイルに書き出すようにした

--- github_tool.py
+++ github_tool.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import urllib.request
 
 
@@ -14,39 +15,58 @@ def download_asset_url_from_github(repository_author,
     :param file_name:
     :return:
     """
+
+    logging.info('download asset url from github')
+
     api_base_url = "https://api.github.com/repos/{}/{}".format(repository_author, repository_name)
 
     if release_tag is None:
+        logging.info('release_tag=%s', release_tag)
         response = urllib.request.urlopen("{}/releases/latest".format(api_base_url))
+        logging.info('finish urlopen')
         content = json.loads(response.read().decode('utf8'))
+        logging.info('content=%s', content)
         release_tag = content["tag_name"]
+        logging.info('release_tag=%s', release_tag)
 
     if file_name is None:
+        logging.info('file_name=%s', file_name)
         response = urllib.request.urlopen("{}/releases/tags/{}".format(api_base_url, release_tag))
+        logging.info('finish urlopen')
         content = json.loads(response.read().decode('utf8'))
+        logging.info('content=%s', content)
         file_name = content["assets"][0]["name"]
+        logging.info('file_name=%s', file_name)
 
-    return "{}/{}/{}/releases/download/{}/{}".format("https://github.com",
-                                                     repository_author,
-                                                     repository_name,
-                                                     release_tag,
-                                                     file_name
-                                                     )
+    url = "{}/{}/{}/releases/download/{}/{}".format("https://github.com",
+                                                    repository_author,
+                                                    repository_name,
+                                                    release_tag,
+                                                    file_name)
+
+    logging.info('url=%s', url)
+
+    return url
 
 
 def download_asset_from_github(repository_author,
                                repository_name,
-                               out_file_path,
+                               out_file,
                                release_tag=None,
                                file_name=None):
+    logging.info('download asset from github')
+
     request_url = download_asset_url_from_github(repository_author,
                                                  repository_name,
                                                  release_tag,
                                                  file_name)
 
+    logging.info('request_url=%s', request_url)
+
     req = urllib.request.Request(request_url)
 
-    with open(out_file_path, "wb", errors='ignore') as my_file:
-        my_file.write(urllib.request.urlopen(req).read())
+    logging.info('create request')
 
-    return out_file_path
+    out_file.write(urllib.request.urlopen(req).read())
+
+    logging.info('finish urlopen')
